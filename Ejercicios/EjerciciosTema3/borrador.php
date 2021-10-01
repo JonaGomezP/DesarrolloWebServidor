23cd822726ffc8016e8af032fc25cb574403d899
Jonatan Gomez <jgp0007@alu.medac.es>
2021-10-01 10:40:30 +0200

    ejercicios tema 3 actualizados

--- Ejercicios/EjerciciosTema3/borrador.php
+++ Ejercicios/EjerciciosTema3/borrador.php
@@ -19,29 +19,76 @@
     /*   TU CÓDIGO   */
 
     //Ejercicio 1
+    //OPCION 1
     print_r("<b>Ejercicio 1</b></br>");
-    if ($ent1 >= 'true' && $ent3 >= 'true') {
+    if (strtolower($ent1) == 'true' && strtolower($ent3) == 'true') {
         print_r("Verdadero");
     } else {
         print_r("Falso");
     }
     print_r("</br>");
 
+
+    //OPCION 2
+    /*
+    function str_to_bool($ent)
+    {
+        $Bool = array(
+            "true" => true,
+            "false" => false
+        );
+
+
+        return $Bool[$ent];
+    }
+
+    function str_to_bool2($ent)
+    {
+
+        return array(
+            "true" => true,
+            "false" => false
+        )[$ent];
+    }
+
+    $b = str_to_bool($ent2);
+    */
+
+
     //Ejercicio 2
     print_r("<b>Ejercicio 2</b></br>");
-    if ($ent1 > 100) {
-        print_r($ent1 . " es mayor que 100</br>");
-    } elseif ($ent1 >= 50 && $ent1 <= 100) {
-        print_r($ent1 . " está entre 50 y 100</br>");
-    } elseif ($ent1 >= 25 && $ent1 < 50) {
-        print_r($ent1 . " está entre 25 y 50</br>");
-    } else {
-        print_r($ent1 . " es menor de 25</br>");
+    if(is_numeric($ent1)){
+        if ($ent1 > 100) {
+            print_r($ent1 . " es mayor que 100</br>");
+        } elseif ($ent1 >= 50 && $ent1 <= 100) {
+            print_r($ent1 . " está entre 50 y 100</br>");
+        } elseif ($ent1 >= 25 && $ent1 < 50) {
+            print_r($ent1 . " está entre 25 y 50</br>");
+        } else {
+            print_r($ent1 . " es menor de 25</br>");
+        }
+    }
+     else {
+        print_r("No es un número</br>");
     }
 
     //Ejercicio 3
+
     print_r("<b>Ejercicio 3</b></br>");
-    if (strtolower($ent1) == 'perro') {
+
+    echo "Opción a)</br>";
+    switch (strtolower($ent1)) {
+        case 'perro':
+        case 'gato':
+        case 'loro':
+            echo "Es un ".$ent1."</br>";
+            break;
+        default:
+        echo "Es un mutante</br>";
+    }
+
+    echo "Opción b)</br>";
+    if (strtolower($ent1) == "perro") {
         print 'Es un perro</br>';
     } elseif (strtolower($ent1) == 'gato') {
         print 'Es un gato</br>';
@@ -56,39 +103,102 @@
 
     switch ($ent1) {
         case 'perro':
-            print 'Es un perro';
+            print 'Es un perro</br>';
             break;
         case 'gato':
-            print 'Es un gato';
+            print 'Es un gato</br>';
             break;
         case 'loro':
-            print 'Es un loro';
+            print 'Es un loro</br>';
             break;
         default:
-            print 'Es un  mutante';
+            print 'Es un  mutante</br>';
             break;
     }
 
     //Ejercicio 5
     print_r("<b>Ejercicio 5</b></br>");
 
-    if (is_numeric($ent1)&&is_numeric($ent3)) {
+    if (is_numeric($ent1) && is_numeric($ent3)) {
         if ($ent2 == 'v1') {
             $resultado = $ent1 + $ent3;
-            print $resultado;
-        }elseif ($ent2 == 'v2') {
+            print $resultado."</br>";
+        } elseif ($ent2 == 'v2') {
             $resultado = $ent1 - $ent3;
-            print $resultado;
-        }elseif ($ent2 == 'v3') {
+            print $resultado."</br>";
+        } elseif ($ent2 == 'v3') {
             $resultado = $ent1 * $ent3;
-            print $resultado;
+            print $resultado."</br>";
         } else {
             $resultado = $ent1 / $ent3;
-            print $resultado;
+            print $resultado."</br>";
         }
     } else {
-        print 'No son números';
+        print 'No son números</br>';
     }
+
+    //Ejercicio 6
+    print_r("<b>Ejercicio 6</b></br>");
+
+    $dat1 = $_GET['dato1'];
+    $dat2 = $_GET['dato2'];
+    $dat3 = $_GET['dato3'];
+    switch ($dat2) {
+        case 'v1':
+            if (($dat1 == "true" && $dat3 == "true") || ($dat1 == "false" && $dat3 == "false")) {
+                echo "verdadero";
+            } else {
+                echo "falso";
+            }
+            break;
+
+        case 'v2':
+            if ($dat1 == "true" || $dat3 == "true") {
+                echo "verdadero";
+            } else {
+                echo "falso";
+            }
+            break;
+
+        case 'v3':
+            if (($dat1 == "true" && $dat3 == "true") || ($dat1 == "false" && $dat3 == "false")) {
+                echo "false";
+            } else {
+                echo "true";
+            }
+            break;
+
+        default:
+            if ($dat1 == "true") {
+                echo $dat1 . " cambiado es false";
+            } else if ($dat1 == "false") {
+                echo $dat1 . " cambiado es true";
+            }
+            echo "<br>";
+
+            if ($dat3 == "true") {
+                echo $dat3 . " cambiado es false";
+            } else if ($dat3 == "false") {
+                echo $dat3 . " cambiado es true";
+            }
+            break;
+    }
+    echo "<br>";
+
+    print_r("<b>Ejercicio 7</b></br>");
+
+    $arrayString = explode(' ',$ent1);
+
+    echo "Opcion a)</br>";
+    for ($i=0; $i < sizeof($arrayString) ; $i++) { 
+        echo ($arrayString[$i]."</br>");
+    }
+    
+    echo "Opcion b)</br>";
+    foreach ($arrayString as $value) {
+        echo ($value."</br>");
+    }
+
     ?>
 
 </body>
